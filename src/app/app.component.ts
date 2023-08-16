e1459f7d39dac71b2db78d3dd818947227458914
Sophat <sophattann24@gmail.com>
2023-08-16 10:27:02 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'PP_TANN_SOPHAT_ANGULAR_MINI_PROJECT';
+}
